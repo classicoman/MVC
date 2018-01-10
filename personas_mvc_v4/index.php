6ac6f28409ac6718d21b2375f47ea544bb1b72e3
Toni <tamengual9@gmail.com>
2018-01-10 10:48:56 +0100

    9/1 10.45h

---
+++ personas_mvc_v4/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once("db/db.php");
+
+require_once("controllers/personas_controller.php");
+require_once("controllers/coches_controller.php");
+
+
+//Funciones para el controlador frontal
+//F//https://victorroblesweb.es/2014/07/15/ejemplo-php-poo-mvc/
+include 'core/ControladorFrontal.func.php';
+
+
+/**
+ * CONTROLADOR FRONTAL
+ */
+if(isset($_GET["controller"])){
+    $controllerObj=cargarControlador($_GET["controller"]);
+    lanzarAccion($controllerObj);
+}else {
+    $controllerObj=cargarControlador(CONTROLADOR_DEFECTO);
+    lanzarAccion($controllerObj);
+}
+
+
+?>
